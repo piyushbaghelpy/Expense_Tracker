1ce2dc64e498a7ae312c7788fdb6bf1f03558f6a
Piyush Swarup Baghel <piyushswarupbaghel@gmail.com>
2024-09-11 11:12:39 +0530

    final

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import CurrentBalance, TrackingHistory, RequestLogs
+
+admin.site.register(CurrentBalance)
+admin.site.register(TrackingHistory)
+admin.site.register(RequestLogs)
